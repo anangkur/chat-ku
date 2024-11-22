068b15751b957a2b89622b080a6c5f21cf780821
Anang Kurniawan <anangk97@gmail.com>
2024-11-22 22:28:31 +0700

    Initial Commit

---
+++ chatku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chatku
+//
+//  Created by Anang Kurniawan on 22/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ chatkuTests/chatkuTests.swift
@@ -0,0 +1,17 @@
+//
+//  chatkuTests.swift
+//  chatkuTests
+//
+//  Created by Anang Kurniawan on 22/11/24.
+//
+
+import Testing
+@testable import chatku
+
+struct chatkuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
