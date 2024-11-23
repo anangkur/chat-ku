8170144538f123869d1743613b4232ea2065c750
Anang Kurniawan <anangk97@gmail.com>
2024-11-23 17:31:17 +0700

    feat: implement ClTypinglabel

--- chatku/ViewController.swift
+++ chatku/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleLabel.text = "Chat Ku"
     }
 
 
