9a79e3ba9766c04ea1231a4901df39162a014770
Anang Kurniawan <anangk97@gmail.com>
2024-11-28 10:08:17 +0700

    feat: implement CLTypingLabel through cocoapod

---
+++ Pods/Target Support Files/Pods-chatku/Pods-chatku-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chatku : NSObject
+@end
+@implementation PodsDummy_Pods_chatku
+@end
---
+++ Pods/Target Support Files/Pods-chatkuTests/Pods-chatkuTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chatkuTests : NSObject
+@end
+@implementation PodsDummy_Pods_chatkuTests
+@end
